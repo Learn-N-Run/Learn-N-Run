bb8da3a369e98ebcd3d86cd938b8a472f3ebc378
Bigfoot <yume7168@naver.com>
2019-07-12 18:17:11 +0900

    ※ 1차 배포 (배포영역 : 전부)  1. dao, controller 클래스 파일 이름 에러 수정  2. 가이드 제작 완료    - GIT 기초 운용방법  3. CDN 소스코드 에러 수정(부트스트랩+jquery)  4. ※ 2차 배포 예정 사항    - header.jsp, footer.jsp 수정 배포 예정

---
+++ Learn_N_Run/src/article/one/dao/UserDAIOImpl.java
@@ -0,0 +1,133 @@
+package artcle.one.dao;
+
+import java.util.List;
+
+import dto.BuyerDTO;
+import dto.CouponDTO;
+import dto.JjimDTO;
+import dto.MessageDTO;
+import dto.UserDTO;
+
+public class UserDAIOImpl implements UserDAO{
+
+	@Override
+	public void addUser(UserDTO dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int updateUser(UserDTO dto, String pass) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void delUser(UserDTO dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int idCheck(String id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int userCheck(String id, String pass) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int emailCheck(String email) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public UserDTO UserInfo(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String creatorCheck(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateCreator(UserDTO bean) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void sendMessage(MessageDTO dto, String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendMail(String receiver, String code_check) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int countReadMessage(String id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<MessageDTO> getMessage(MessageDTO bean, String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MessageDTO getMessageInfo(int MessageNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delMessage(int no) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public CouponDTO myCouponInfo(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int buyClass(int classNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public CouponDTO CouponClass(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addBuy(BuyerDTO bean, String id, int classNo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public JjimDTO getJjim(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
