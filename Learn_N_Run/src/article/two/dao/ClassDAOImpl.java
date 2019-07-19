3bb7be77d295bac59b3c6ce854c2cdc406253ec1
gowso22 <gowso89@naver.com>
2019-07-19 16:39:17 +0900

    aw,ea,w

---
+++ Learn_N_Run/src/article/two/dao/ClassDAOImpl.java
@@ -0,0 +1,85 @@
+package article.two.dao;
+
+import java.util.List;
+
+import dto.BuyerDTO;
+import dto.CategoryDTO;
+import dto.ClassDTO;
+import dto.CurriculumDTO;
+
+public class ClassDAOImpl implements ClassDAO{
+
+	@Override
+	public void addClass(String id, ClassDTO dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateClass(ClassDTO dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertCurriculum(int classNo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delCurriculum(int curriculmNo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ClassDTO updateCurriculum(CurriculumDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void uploadClass(ClassDTO dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ClassDTO ModifyClass(ClassDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CurriculumDTO ModifyCurriculum(CurriculumDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delClass(ClassDTO dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ClassDTO classInfo(ClassDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<CategoryDTO> categoryInfo(CategoryDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BuyerDTO> myClassList(BuyerDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
