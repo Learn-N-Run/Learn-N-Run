5e8b9351c42ab1fd06be437f893291cf5e658281
Bigfoot <yume7168@naver.com>
2019-08-01 01:37:31 +0900

    스프링 3.x버전으로 낮춤 ...;;; 학원에서 쓰는 라이브러리로 대체함 dispatherservlet이 이상함 web.xml
    (당장은 무리 일것 같음)

--- Learn_N_Run/src/article/three/controller/TH_Controller.java
+++ Learn_N_Run/src/article/three/controller/TH_Controller.java
@@ -1,12 +1,15 @@
 package article.three.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-//@RequestMapping("/test")
-//@Controller
+
+@Controller("thcontroller")
 public class TH_Controller{
 
-//	@RequestMapping("/success")
+	@RequestMapping(value = "/success.do",method = RequestMethod.GET )
 	public ModelAndView testdsfsd() {
 		
 		ModelAndView mav = new ModelAndView();
