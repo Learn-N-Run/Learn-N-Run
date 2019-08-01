bd211aac631be47a2ccfb147959ab9c392328cd3
Bigfoot <yume7168@naver.com>
2019-08-01 18:51:19 +0900

    #1 구현작업 controller: servlet으로 구현예정 DAO: 인터페이스 상속만 준비

--- Learn_N_Run/src/article/three/controller/TH_Controller.java
+++ Learn_N_Run/src/article/three/controller/TH_Controller.java
@@ -1,21 +1,95 @@
 package article.three.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@Controller("thcontroller")
-public class TH_Controller{
+@WebServlet("*.po")
+public class TH_Controller extends HttpServlet{
 
-	@RequestMapping(value = "/success.do",method = RequestMethod.GET )
-	public ModelAndView testdsfsd() {
+	private static final long serialVersionUID = 1L;
+	private StringBuffer ctrlConsole;
+
+/* String 생성자 최소화를 이유로  스트링버퍼로 문자열을 추가하여 콘솔확인하기 위한 메소드*/
+	private void viewConsole(String ...strings) {
+
+		ctrlConsole = new StringBuffer();
+
+		for (String string : strings) {
+			ctrlConsole.append(string);
+		}
 		
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("key","key생성완료");
-		mav.setViewName("index");
-		return mav;
+		System.out.print(ctrlConsole);
+	}
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		viewConsole("accepted method : ", "GET");
+		doHandler(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		viewConsole("accepted method : ", "POST");
+		doHandler(request, response);
+	}
+	
+	protected void doHandler(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+/* 한글처리 */
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+
+/* 커맨드 추출 */
+		String uri = request.getRequestURI();
+		String conpath = request.getContextPath();
+		String command = uri.substring(conpath.length());
+		viewConsole("command : ",command);
+		
+/* 경로 선언 */
+		String path = "";
+
+		if (command.equals("여기에넣으세요")) {
+			
+			
+			path="index.jsp";
+		}else if (command.equals("여기에넣으세요")) {
+			
+			path="";
+		}else if (command.equals("여기에넣으세요")) {
+			
+			path="";
+		}else if (command.equals("여기에넣으세요")) {
+			
+			path="";
+		}else if (command.equals("여기에넣으세요")) {
+			
+			path="";
+		}else if (command.equals("여기에넣으세요")) {
+			
+			path="";
+		}else if (command.equals("여기에넣으세요")) {
+			
+			path="";
+		}else if (command.equals("여기에넣으세요")) {
+			
+			path="";
+		}else if (command.equals("여기에넣으세요")) {
+			
+			path="";
+		}
+				
+		RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+		dispatcher.forward(request, response);
+	}
+
+//TODO: test용
+	public static void main(String[] args) {
+		TH_Controller th = new TH_Controller();
+		th.viewConsole("accepted method : ", "GET");
 	}
 	
 }
---
+++ Learn_N_Run/src/article/three/dao/MainReadDAOimpl.java
@@ -0,0 +1,31 @@
+package article.three.dao;
+
+import java.util.List;
+
+import dto.BuyerDTO;
+import dto.ClassDTO;
+import dto.CurriculumDTO;
+
+public class MainReadDAOimpl implements MainReadDAO {
+
+	@Override
+	public List<BuyerDTO> top5Info(BuyerDTO bean) {
+		return null;
+	}
+
+	@Override
+	public List<ClassDTO> getCategoryClass(ClassDTO bean) {
+		return null;
+	}
+
+	@Override
+	public List<CurriculumDTO> getCurriculum(CurriculumDTO dto) {
+		return null;
+	}
+
+	@Override
+	public CurriculumDTO getVideo(CurriculumDTO dto, int subjectNo) {
+		return null;
+	}
+
+}
